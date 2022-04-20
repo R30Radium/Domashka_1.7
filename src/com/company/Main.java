c8ac7a3205afe887beb86ee2edf46d2e89d96c01
R30Radium <r30radium@gmail.ru>
2022-04-20 21:23:23 +0400

    kommit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,32 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Задание 1
+
+
+        String firstName = " Ivan ";
+        String middleName = " Ivanov ";
+        String lastName = " Ivanovich ";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        //Задание 2
+
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullNameUpper);
+
+        //Задание 3
+
+        StringBuilder fullNameSemen = new StringBuilder("Ivanov Ivan Ivanovich");
+        fullNameSemen.delete(0, 21);
+        fullNameSemen.append("Иванов Семён Семёнович");
+        System.out.println(fullNameSemen);
+
+        fullName = "Иванов Семён Семёнович";
+
+        String fullNameSemenChar = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника  —  " + fullNameSemenChar);
+
     }
 }
