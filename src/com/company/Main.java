ce18220731c20d57f66fc7c199cb634fc73d9d38
R30Radium <r30radium@gmail.ru>
2022-04-20 21:28:07 +0400

    kommit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         //Задание 1
-        //*RRR
+        //*RRRRr33
 
         String firstName = " Ivan ";
         String middleName = " Ivanov ";
